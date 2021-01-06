3c14efb91282874326c43480af99bc21faef947c
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-05 20:53:28 -0600

    counting directories in first level

--- count.py
+++ count.py
@@ -9,16 +9,10 @@ def count_directories():
     # Use of scandir() to display all the files (excluding directories)
     # in the given path that don’t start with '.';
     # the entry.is_file() call will generally not make an additional system call
-    path = settings.DIR_PATH
+    path_to_dirs = settings.DIR_PATH
 
-    files_list = list()
-
-    with os.scandir(path) as it:
-        for entry in it:
-            if not entry.name.startswith('.') and entry.is_file():
-                files_list.append(entry)
-
-    print(len(files_list))
+    directories_list = os.listdir(path_to_dirs)
+    print(len(directories_list))
 
 
 ###--- DRIVER CODE ---###
